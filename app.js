9039782d27bf9ea0e0a8919eb0b6e75c6b18435d
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-08-17 16:29:31 +0200

    App setup done

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-var express      = require("express"),
-    mongoose     = require("mongoose");
+var express                = require("express"),
+    mongoose               = require("mongoose"),
+    passport               = require("passport"),
+    bodyParser             = require("body-parser"),
+    User                   =
+    require("./models/user"),
+    LocalStrategy          = require("passport-local"),
+    passportLocalMongoose  = require("passport-local-mongoose");
+
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
-
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+  secret: "My name is Patryk",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -17,4 +34,4 @@ app.get("/secret", function(req, res){
 
 app.listen(3000, function(){
   console.log("Server started....");
-})
+});
